2a997300126b28d6957c0427bbbd2cfd049d591a
daino3 <dainovu3@gmail.com>
2018-12-29 12:06:02 -0600

    adding photo model

--- wedding/models.py
+++ wedding/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
+
+class Photo(models.Model):
+    description = models.CharField(max_length=500, null=True)
+    name = models.CharField(max_length=250, null=False)
+
+    photo = models.ImageField(upload_to='photos', max_length=200, storage=FileSystemStorage())
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
