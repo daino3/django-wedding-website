677215df46a5d44549d662063fd73870ee545eab
daino3 <dainovu3@gmail.com>
2018-12-31 16:41:39 -0500

    added embedded google map

--- wedding/views.py
+++ wedding/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 from .models import Photo
 from .serializers import PhotoSerializer
+from datetime import date
 
 
 def home(request):
@@ -10,8 +11,14 @@ def home(request):
 
     photo_data = PhotoSerializer(photos, many=True).data
 
+    wedding_day = date(2019, 9, 15)
+    today = date.today()
+
+    days_until_wedding = (wedding_day - today).days
+
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
         'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
-        'photos': {photo['name']: photo for photo in photo_data}
+        'photos': {photo['name']: photo for photo in photo_data},
+        'days_until_wedding': days_until_wedding,
     })
